9a2c5ea88725afabc9b88f4444fc3eec795261f2
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-08-16 16:27:13 +0900

    feat: chat ui

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,3 +1,114 @@
+<script setup lang="ts">
+import { nanoid } from 'nanoid'
+import type { Message, User } from '@/types'
+
+const props = withDefaults(
+  defineProps<{
+    messages: Message[]
+    users: User[]
+    me: User
+    usersTyping?: User[]
+  }>(),
+  {
+    usersTyping: () => [],
+  },
+)
+
+const emit = defineEmits<{
+  (e: 'newMessage', payload: Message): void
+}>()
+
+const textMessage = ref('')
+function sendMessage() {
+  emit('newMessage', {
+    id: nanoid(),
+    userId: props.me.id,
+    createdAt: new Date(),
+    text: textMessage.value,
+  })
+  textMessage.value = ''
+}
+
+const isOpen = ref(false)
+
+function getUser(id: string) {
+  return props.users.find((user) => user.id === id)
+}
+
+const messageBox = ref<HTMLElement>()
+watch(
+  () => props.messages.length,
+  async () => {
+    await nextTick()
+    if (messageBox.value)
+      messageBox.value.scrollTop = messageBox.value.scrollHeight
+  },
+)
+</script>
+
 <template>
-  <!-- Code out the chatbox for Exercise 1 here -->
+  <div class="fixed bottom-[10px] right-[10px]">
+    <button
+      v-show="!isOpen"
+      class="rounded bg-blue-500 p-3"
+      @click="isOpen = true"
+    >
+      <IconChat class="h-8 w-8 text-white" />
+    </button>
+    <div
+      v-if="isOpen"
+      class="box w-[450px] overflow-hidden rounded bg-gray-300 dark:bg-gray-800"
+    >
+      <header
+        class="flex items-center justify-between bg-gray-200 px-4 dark:bg-gray-900"
+      >
+        Customer Support Chat
+        <button class="p-4 pr-0" @click="isOpen = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </header>
+      <!-- messages -->
+      <div ref="messageBox" class="messages max-h-[80vh] overflow-y-scroll p-4">
+        <ChatBubble
+          v-for="message in messages"
+          :key="message.id"
+          :user="getUser(message.userId)"
+          :message="message"
+          :my-message="message.userId === me.id"
+        />
+
+        <ChatBubble v-for="user in usersTyping" :key="user.id" :user="user">
+          <AppLoading />
+        </ChatBubble>
+      </div>
+      <footer class="p-2">
+        <input
+          v-model="textMessage"
+          type="text"
+          class="input block w-full px-2"
+          placeholder="Type your message"
+          @keypress.enter.exact.prevent="sendMessage"
+        />
+        <div class="h-6 px-2 py-1 text-sm italic">
+          <span v-if="usersTyping.length">
+            {{ usersTyping.map((user) => user.name).join(' and ') }}
+            {{ usersTyping.length === 1 ? 'is' : 'are' }} typing
+          </span>
+        </div>
+      </footer>
+    </div>
+  </div>
 </template>
---
+++ components/ChatBubble.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+// @ts-expect-error no types for markdownit
+import Markdown from 'vue3-markdown-it'
+import type { Message, User } from '@/types'
+
+const props = defineProps<{
+  user?: User
+  message?: Message
+  myMessage?: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="chat"
+    :class="{
+      'chat-end': myMessage,
+      'chat-start': !myMessage,
+    }"
+  >
+    <div class="avatar chat-image">
+      <div class="w-10 rounded-full">
+        <img :src="user?.avatar" alt="avatar" />
+      </div>
+    </div>
+    <div class="chat-header mb-2">
+      <strong>{{ user?.name }}</strong>
+      &nbsp;
+      <time v-if="message" class="text-xs opacity-50">{{
+        useTimeAgo(message?.createdAt)
+      }}</time>
+    </div>
+    <div
+      class="chat-bubble prose prose-sm w-full max-w-max bg-white py-0 dark:bg-gray-900"
+      :class="{
+        'bg-gray-600 text-white dark:bg-gray-700 dark:text-inherit': myMessage,
+      }"
+    >
+      <slot>
+        <Markdown :source="message?.text" class="w-full" />
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:deep(code) {
+  background: none;
+  @apply w-full overflow-x-auto;
+}
+:deep(pre) {
+  @apply w-full overflow-x-auto dark:bg-[rgba(0,0,0,.3)];
+}
+</style>
